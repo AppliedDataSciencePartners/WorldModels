0f742d8413f8e666ec7ba1e23ae81d75114c5e8d
David Foster <david@applied-data.science>
2018-04-18 14:48:20 +0100

    added render option to 01_generate-data.py

--- 01_generate_data.py
+++ 01_generate_data.py
@@ -1,14 +1,13 @@
 #xvfb-run -s "-screen 0 1400x900x24" python generate_data.py car-racing --total_episodes 200 --start_batch 0 --time_steps 300
 
-#import gym
 import numpy as np
 import random
 import config
+import matplotlib.pyplot as plt
 
 from env import make_env
 
 import argparse
-
 
 def main(args):
 
@@ -16,36 +15,46 @@ def main(args):
     total_episodes = args.total_episodes
     start_batch = args.start_batch
     time_steps = args.time_steps
-
-    obs_data = []
-    action_data = []
+    render = args.render
 
     env = make_env(env_name)
     s = 0
     batch = start_batch
 
+
     while s < total_episodes:
+        obs_data = []
+        action_data = []
 
         for i_episode in range(200):
             print('-----')
             observation = env.reset()
+            observation = config.adjust_obs(observation)
+            if render:
+                plt.imshow(observation)
+                plt.show()
+
             env.render()
             done = False
             action = env.action_space.sample()
             t = 0
             obs_sequence = []
             action_sequence = []
-            while t < time_steps:
+
+            while t < time_steps and not done:
                 t = t + 1
                 
                 action = config.generate_data_action(t, action)
-                observation = config.adjust_obs(observation)
-                      
+                
                 obs_sequence.append(observation)
                 action_sequence.append(action)
-                
+
                 observation, reward, done, info = env.step(action)
-            
+                observation = config.adjust_obs(observation)
+
+                if render:
+                    env.render()
+
             obs_data.append(obs_sequence)
             action_data.append(action_sequence)
             
@@ -60,15 +69,14 @@ def main(args):
 
         batch = batch + 1
 
-        obs_data = []
-        action_data = []
-
 if __name__ == "__main__":
   parser = argparse.ArgumentParser(description=('Create new training data'))
   parser.add_argument('env_name', type=str, help='name of environment')
   parser.add_argument('--total_episodes', type=int, default = 200, help='total number of episodes to generate')
   parser.add_argument('--start_batch', type=int, default = 0, help='start_batch number')
   parser.add_argument('--time_steps', type=int, default = 300, help='how many timesteps at start of episode?')
+  parser.add_argument('--render', action='store_true', help='render the env as data is generated')
+
 
   args = parser.parse_args()
   main(args)
