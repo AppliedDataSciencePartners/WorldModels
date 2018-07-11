2260c3c76cc63aaf45d1bd1f15e48ee63f3e3c47
David Foster <david@applied-data.science>
2018-07-11 22:27:45 +0100

    reward

--- config.py
+++ config.py
@@ -5,25 +5,25 @@ train_envs = ['car_racing']
 test_envs = ['car_racing']
 
 def generate_data_action(t, current_action):
-#     a = env.action_space.sample()
-#     return a
-    if t < 10:
-        return np.array([0,1,0])
+    # a = env.action_space.sample()
+    return np.array([random.random()*2-1,random.random(),random.random()])
+    # if t < 10:
+    #     return np.array([0,1,0])
     
-    if t % 5 > 0:
-        return current_action
+    # if t % 5 > 0:
+    #     return current_action
 
-    rn = random.randint(0,9)
-    if rn in [0]:
-        return np.array([0,0,0])
-    if rn in [1,2,3,4]:
-        return np.array([0,random.random(),0])
-    if rn in [5,6,7]:
-        return np.array([-random.random(),0,0])
-    if rn in [8]:
-        return np.array([random.random(),0,0])
-    if rn in [9]:
-        return np.array([0,0,random.random()])
+    # rn = random.randint(0,9)
+    # if rn in [0]:
+    #     return np.array([0,0,0])
+    # if rn in [1,2,3,4]:
+    #     return np.array([0,random.random(),0])
+    # if rn in [5,6,7]:
+    #     return np.array([-random.random(),0,0])
+    # if rn in [8]:
+    #     return np.array([random.random(),0,0])
+    # if rn in [9]:
+    #     return np.array([0,0,random.random()])
 
 
 def adjust_obs(obs):
