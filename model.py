06d1d19bef3c43cd48a5478f2e58826b6526b7f6
David Foster <david@applied-data.science>
2018-07-26 15:01:29 +0100

    new

--- model.py
+++ model.py
@@ -214,7 +214,6 @@ def simulate(model, num_episode=5, seed=-1, max_len=-1, generate_data_mode = Fal
         obs = config.adjust_obs(obs)
         reward = config.adjust_reward(reward)
 
-
       if render_mode:
         model.env.render("human")
         if RENDER_DELAY:
