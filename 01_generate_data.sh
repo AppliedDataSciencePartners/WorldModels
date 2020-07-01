5c2e7a5fa999abaf4d6cf1acbcd39c7dec438b0d
David Foster <david@applied-data.science>
2020-07-01 11:26:20 +0100

    bug in xvfb code in generate data

--- 01_generate_data.sh
+++ 01_generate_data.sh
@@ -4,7 +4,7 @@ for i in `seq 1 $2`;
 do
   echo worker $i
   # on cloud with no screen:
-  # xvfb-run -a -s "-screen 0 1400x900x24 +extension RANDR" -- python extract.py $1 --total_episodes $2 --time_steps $3 &
+  # xvfb-run -a -s "-screen 0 1400x900x24" python 01_generate_data.py $1 --total_episodes $3 --time_steps $4 &
 
   # on local machine with screen for debugging:
   python 01_generate_data.py $1 --total_episodes $3 --time_steps $4 --render $5 --action_refresh_rate $6 &
