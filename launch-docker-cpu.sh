62bf555fb73b16e8759b03a7b0cd9fc7744bb7c8
David Foster <david@applied-data.science>
2020-07-11 23:56:10 +0100

    changes to dockerfiles

--- launch-docker-cpu.sh
+++ launch-docker-cpu.sh
@@ -1,8 +1,8 @@
-# USAGE - ./launch-docker-cpu.sh {abs-path-to-GDL-code}
+# USAGE - ./launch-docker-cpu.sh {abs-path-to-WorldModels-code}
 # - eg. to run from current directory:
 #     ./launch-docker-cpu.sh $(pwd)
 if [[ "$OSTYPE" == "darwin"* ]]; then
-    docker run --rm -p 8888:8888 -it -v $1:/Worldmodels gdl-image-cpu
+    docker run --rm -p 8888:8888 -it -v $1:/Worldmodels worldmodels-image-cpu
 else
-    docker run --rm --network=host -it -v $1:/Worldmodels gdl-image-cpu
+    docker run --rm --network=host -it -v $1:/Worldmodels worldmodels-image-cpu
 fi
--- launch-docker-gpu.sh
+++ launch-docker-gpu.sh
@@ -1,2 +1,2 @@
-# USAGE - ./launch-docker-gpu.sh {abs-path-to-GDL-code}
-docker run --rm --runtime=nvidia --network=host -it -v $1:/Worldmodels gdl-image
+# USAGE - ./launch-docker-gpu.sh {abs-path-to-WorldModels-code}
+docker run --rm --runtime=nvidia --network=host -it -v $1:/Worldmodels worldmodels-image
